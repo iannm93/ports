04a781157ea0faafe9f5b02912716f7a80a4564e
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-03-26 03:30:16 -0700

    scroll func on index. opacity fade gallery

--- Kalis/script.js
+++ Kalis/script.js
@@ -1,21 +1,32 @@
 let cardOne = document.getElementById("card1") 
-let images = document.images
-let illustrations = document.getElementById("ui-ux");
+let images = document.getElementById("img1") 
+
+// let images = document.images
+let illustrations = document.querySelector(".purp");
+let intervalId;
+let temp = window.getComputedStyle(illustrations).getPropertyValue("opacity");
+let loop = ()=>{
+        if (window.getComputedStyle(illustrations).getPropertyValue("opacity") > 0){
+illustrations.style.opacity - .2
+console.log(illustrations.style.opacity) 
+
+   
+        }
 
 
-
-let slideOne = ()=>{
-    for (let i =1; i <images.length; i ++){
-        images[i].style.cssText=`transform-origin: -200% -200%;
-                              transform: rotate(35deg);
-                              transition: .5s ease-in`
-    }
-
-    // console.log(images.length)
+// console.log(images.length)
     // images.style.cssText = `transform-origin: -200% -200%;
     //                         transform: rotate(35deg);
     //                         transition: .5s ease-in `
-                            
+    
 }
 // slideOne()
+let slideOne = ()=>{
+  setInterval(loop, 100)
+} 
+    
+        // images[i].style.cssText=`transform-origin: -200% -200%;
+        //                       transform: rotate(35deg);
+        //                       transition: .5s ease-in`
+    
 illustrations.addEventListener("click", slideOne)
