528a93e5eb56250122890eebc82914ed6e299ed1
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-03-26 01:48:53 -0700

    working on slide in animation for galleries

--- Kalis/script.js
+++ Kalis/script.js
@@ -1,5 +1,21 @@
 let cardOne = document.getElementById("card1") 
-function goToOne(){
-    window.open("knotsofunny.html")
+let images = document.images
+let illustrations = document.getElementById("ui-ux");
+
+
+
+let slideOne = ()=>{
+    for (let i =1; i <images.length; i ++){
+        images[i].style.cssText=`transform-origin: -200% -200%;
+                              transform: rotate(35deg);
+                              transition: .5s ease-in`
+    }
+
+    // console.log(images.length)
+    // images.style.cssText = `transform-origin: -200% -200%;
+    //                         transform: rotate(35deg);
+    //                         transition: .5s ease-in `
+                            
 }
-cardOne.addEventListener("click",goToOne)
+// slideOne()
+illustrations.addEventListener("click", slideOne)
