e3360c90fc3021079fc583d9522a45c9244bf6b4
Ian Harris <ianmharris93@gmail.com>
2023-03-26 18:27:37 -0700

    opacity for grid fade funct

--- Kalis/script.js
+++ Kalis/script.js
@@ -1,28 +1,66 @@
 let cardOne = document.getElementById("card1") 
-let images = document.getElementById("img1") 
+let images = document.querySelector(".img7") 
 
 // let images = document.images
 let illustrations = document.querySelector(".purp");
 let intervalId;
 let temp = window.getComputedStyle(illustrations).getPropertyValue("opacity");
-let loop = ()=>{
-        if (window.getComputedStyle(illustrations).getPropertyValue("opacity") > 0){
-illustrations.style.opacity - .2
-console.log(illustrations.style.opacity) 
+let myInterval;
 
+
+let fadeIn = () =>{
+        let fadeOpacity = Number(
+                window.getComputedStyle(images).getPropertyValue("opacity")
+              );
+              console.log(fadeOpacity)
+              // if the opacity is less than 1,  add .005 to all the below elements' opacity
+              if (fadeOpacity < 1) {
+                fadeOpacity = fadeOpacity + 0.1;
+               images.style.opacity = fadeOpacity
+               console.log(images.style.opacity)
+              }
+}
+
+let newPic = () =>{
    
+                setInterval(fadeIn, 100)
+        
+}
+let loop = function () {
+        console.log("loop rujnning")
+        let fadeOpacity = Number(
+          window.getComputedStyle(images).getPropertyValue("opacity")
+        );
+        console.log(fadeOpacity)
+        // if the opacity is less than 1,  add .005 to all the below elements' opacity
+        if (fadeOpacity > 0) {
+          fadeOpacity = fadeOpacity - 0.1;
+         images.style.opacity = fadeOpacity
+         console.log(images.style.opacity)
+        } else{
+           newPic()
+           clearInterval(myInterval)
+           console.log("cleared")
         }
+     
+      };
 
+    
 
+     
+
+      
+  
 // console.log(images.length)
     // images.style.cssText = `transform-origin: -200% -200%;
     //                         transform: rotate(35deg);
     //                         transition: .5s ease-in `
     
-}
+
 // slideOne()
 let slideOne = ()=>{
-  setInterval(loop, 100)
+ myInterval = setInterval(loop, 50)
+  console.log("test")
 } 
     
         // images[i].style.cssText=`transform-origin: -200% -200%;
