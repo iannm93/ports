93dbd06dc2d69b28d68b911c6570496a9a661b87
Ian Harris <ianmharris93@gmail.com>
2023-03-27 18:35:28 -0700

    opac

--- Kalis/script.js
+++ Kalis/script.js
@@ -1,11 +1,11 @@
 let cardOne = document.getElementById("card1") 
-let images = document.querySelector(".img7") 
+let images = document.querySelector("#grid") 
 
 // let images = document.images
 let illustrations = document.querySelector(".purp");
-let intervalId;
 let temp = window.getComputedStyle(illustrations).getPropertyValue("opacity");
 let myInterval;
+let intervalTwo;
 
 
 let fadeIn = () =>{
@@ -18,16 +18,44 @@ let fadeIn = () =>{
                 fadeOpacity = fadeOpacity + 0.1;
                images.style.opacity = fadeOpacity
                console.log(images.style.opacity)
-              }
+              } else{
+                      console.log("interval two has cleared")        
+                      clearInterval(intervalTwo)
+                } 
 }
 
 let newPic = () =>{
    
-                setInterval(fadeIn, 100)
+        images.innerHTML = `           <a href="https://online.fliphtml5.com/vnnyr/abxc/#p=1"><img  id="img1" class="img7 img-fluid"
+        src="img/dropouts mock.png"
+         alt="Honey Ginger Magazine front, back and spine"></a>
+
+
+<img    id="img2" class="img7 img-fluid"
+    src="img/around the sun album cover.png"
+     alt="Around the Sun album cover alone and on spotify">
+
+<!-- </div> -->
+<!-- <div class="col-lg-4"> -->
+
+
+<img id="img3" class="img7 img-fluid"
+    src="img/Acl ad mock.png"
+    alt="Austin City Limits Ad on Billboard">
+
+
+
+<img id="img4" class="img7 img-fluid"
+    src="img/kss.png"
+    alt="Kalani Surf School Design on white T shirt">
+
+
+`
+                intervalTwo = setInterval(fadeIn, 100)
         
 }
-let loop = function () {
-        console.log("loop rujnning")
+let fadeOut = function () {
+        console.log("loop one running")
         let fadeOpacity = Number(
           window.getComputedStyle(images).getPropertyValue("opacity")
         );
@@ -40,7 +68,8 @@ let loop = function () {
         } else{
            newPic()
            clearInterval(myInterval)
-           console.log("cleared")
+           console.log("interval one has cleared")        
+           
         }
      
       };
@@ -58,13 +87,13 @@ let loop = function () {
     
 
 // slideOne()
-let slideOne = ()=>{
- myInterval = setInterval(loop, 50)
-  console.log("test")
+let startFade = ()=>{
+        console.log("start fade has run")
+ myInterval = setInterval(fadeOut, 50)
 } 
     
         // images[i].style.cssText=`transform-origin: -200% -200%;
         //                       transform: rotate(35deg);
         //                       transition: .5s ease-in`
     
-illustrations.addEventListener("click", slideOne)
+illustrations.addEventListener("click", startFade)
