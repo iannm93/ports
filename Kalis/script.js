6914a2e513f6f432328d1a11579d5574fd7565c4
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-05-09 12:15:19 -0700

    style changes

--- Kalis/script.js
+++ Kalis/script.js
@@ -24,12 +24,20 @@ let updateContent = (targetVal) =>{
     graphic.textContent = "illustration";
     // put all your pictures for the illustrations grid inside the variable below
     images.innerHTML = `
-    <a href="https://online.fliphtml5.com/vnnyr/abxc/#p=1"><img  id="img1" class="img7 img-fluid"src="img/dropouts mock.png" alt="Honey Ginger Magazine front, back and spine"></a>
-      <img id="img2" class="img7 img-fluid" src="img/around the sun album cover.png" alt="Around the Sun album cover alone and on spotify">
-      <img id="img3" class="img7 img-fluid" src="img/Acl ad mock.png" alt="Austin City Limits Ad on Billboard">
-      <img id="img4" class="img7 img-fluid" src="img/kss.png" alt="Kalani Surf School Design on white T shirt">
+    <a href="https://online.fliphtml5.com/vnnyr/abxc/#p=1"><img  id="img1" class="img7 img-fluid"src="img/ppboo.png" alt="Pretty Penguin Book Mockup"></a>
+      <img id="img2" class="img7 img-fluid" src="img/joshua tree cup.png" alt="Joshua Tree Mug Illustration">
+      <img id="img3" class="img7 img-fluid" src="img/feature article 1-images3.png" alt="Girl walking with yoga props">
+      <img id="img3" class="img7 img-fluid" src="img/runaway.png" alt="alien driving">
+      <img id="img3" class="img7 img-fluid" src="img/yogastickersonmat.png" alt="yoga stickers on yoga mat">
+      <img id="img3" class="img7 img-fluid" src="img/lifedrawing.jpg" alt="charcoal drawing of man on floor">
+      <img id="img3" class="img7 img-fluid" src="img/selfport.jpg" alt="charcoal drawing of Kali eating thai food">
+      <img id="img3" class="img7 img-fluid" src="img/untitled_artwork 3.jpg" alt="digital drawing of the water barer">
+      <img id="img3" class="img7 img-fluid" src="img/takemecover.jpg" alt="two fish people in the desert">
+      <img id="img3" class="img7 img-fluid" src="img/starseed cover.png" alt=" Starseed by Kali Coogan with girl floating in space">
+      <img id="img3" class="img7 img-fluid" src="img/morir.png" alt="black and white drawing of a girl">
+      <img id="img3" class="img7 img-fluid" src="img/memorabiliacover.jpg" alt="person with lemon head standing next to person with orange head">
   `;
-    myInterval = setInterval(fade, 60,targetVal, 1)
+    myInterval = setInterval(fade, 100,targetVal, 1)
   } 
   else if (targetVal.includes("graphic")){
     leftLink.innerHTML = `< see illustrations`;
@@ -43,8 +51,14 @@ let updateContent = (targetVal) =>{
       <img id="img2" class="img7 img-fluid" src="img/around the sun album cover.png" alt="Around the Sun album cover alone and on spotify">
       <img id="img3" class="img7 img-fluid" src="img/Acl ad mock.png" alt="Austin City Limits Ad on Billboard">
       <img id="img4" class="img7 img-fluid" src="img/kss.png" alt="Kalani Surf School Design on white T shirt">
+      <img id="img5" class="img7 img-fluid" src="img/kc mock22.png" alt="Kieran and Corrado website design">
+      <img id="img5" class="img7 img-fluid" src="img/honey ginger mag mock.png" alt="honey ginger magazine mock up">
+      <img id="img3" class="img7 img-fluid" src="img/hpbook.png" alt="One thousand magical herbs and fungi book cover with collage of plants">
+      <img id="img3" class="img7 img-fluid" src="img/memo_cd2.png" alt="memorabilia cd design">
+      <img id="img3" class="img7 img-fluid" src="img/krittercatchers_logos-02.png" alt="kritter catchers logo design">
+
 `;
-  myInterval= setInterval(fade, 60, targetVal, 1)
+  myInterval= setInterval(fade, 100, targetVal, 1)
 
   } 
   else if (targetVal.includes("ux")){
@@ -60,16 +74,15 @@ let updateContent = (targetVal) =>{
   ><img
       id="img7"
       class="img-fluid"
-      src="img/ppboo.png"
+      src="img/ksf mock.png"
       class="card-img-top"
-      alt="The Pretty Penguin book cover"
+      alt="Knot So Funny Website Design"
   /></a>
   <div class="card-body">
-  <h5 class="card-title">The Pretty Penguin</h5>
+  <h5 class="card-title">Knot So Funny</h5>
   <p class="card-text">
-      Childrens book. Click
-      <a href="https://heyzine.com/flip-book/08c238dc66.html">here</a> to view.
-  </p>
+  Website Design
+</p>
   </div>
 </div>
 
@@ -78,16 +91,15 @@ let updateContent = (targetVal) =>{
   ><img
       id="img7"
       class="img-fluid"
-      src="img/ppboo.png"
+      src="img/hitrails better.png"
       class="card-img-top"
-      alt="The Pretty Penguin book cover"
+      alt="HiTrails App Design"
   /></a>
   <div class="card-body">
-  <h5 class="card-title">The Pretty Penguin</h5>
+  <h5 class="card-title">HiTrails</h5>
   <p class="card-text">
-      Childrens book. Click
-      <a href="https://heyzine.com/flip-book/08c238dc66.html">here</a> to view.
-  </p>
+   App Redesign
+</p>
   </div>
 </div>
 
@@ -96,20 +108,36 @@ let updateContent = (targetVal) =>{
 ><img
   id="img7"
   class="img-fluid"
-  src="img/ppboo.png"
+  src="img/symphsyn.png"
   class="card-img-top"
-  alt="The Pretty Penguin book cover"
+  alt="Symphonic Synergy kiosk design"
 /></a>
 <div class="card-body">
-<h5 class="card-title">The Pretty Penguin</h5>
+<h5 class="card-title">Music and Me</h5>
 <p class="card-text">
-  Childrens book. Click
-  <a href="https://heyzine.com/flip-book/08c238dc66.html">here</a> to view.
+  Kiosk Design
+</p>
+</div>
+</div>
+
+<div id="card1" class="card mx-auto" style="max-width: 50vh">
+<a href="https://heyzine.com/flip-book/08c238dc66.html"
+><img
+  id="img7"
+  class="img-fluid"
+  src="img/kritter mock.png"
+  class="card-img-top"
+  alt="Kritter Catchers Web Design"
+/></a>
+<div class="card-body">
+<h5 class="card-title">Kritter Catchers</h5>
+<p class="card-text">
+  Logo and Website Design
 </p>
 </div>
 </div>
 `;
-  myInterval= setInterval(fade, 60, targetVal, 1)
+  myInterval= setInterval(fade, 100, targetVal, 1)
 
   } 
   
@@ -125,7 +153,7 @@ let fade = (targetVal, endOpacity)=>{
   if (fadeOpacity !== endOpacity) {
     console.log(fadeOpacity, endOpacity)
     
-    fadeOpacity > endOpacity ? fadeOpacity -= 0.05 : fadeOpacity += 0.05;
+    fadeOpacity > endOpacity ? fadeOpacity -= 0.1 : fadeOpacity += 0.1;
     images.style.opacity = fadeOpacity;
     graphic.style.opacity = fadeOpacity;
     logo.style.opacity = fadeOpacity;
@@ -152,14 +180,14 @@ let linkInterval = (event) => {
       if (targetVal.includes("illustrations")) {
       
         console.log("illus");
-        myInterval = setInterval(fade, 40, targetVal, 0);
+        myInterval = setInterval(fade, 100, targetVal, 0);
       } else if (targetVal.includes("graphic")) {
         console.log("des");
         console.log(targetVal)
-        myInterval = setInterval(fade, 40, targetVal, 0);
+        myInterval = setInterval(fade, 100, targetVal, 0);
       } else if (targetVal.includes("ui")){
         console.log("ui")
-        myInterval = setInterval(fade, 40, targetVal, 0)
+        myInterval = setInterval(fade, 100, targetVal, 0)
       }
   }
   let endTime = dates[-1] +2600
