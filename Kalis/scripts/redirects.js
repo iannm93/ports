e1a859996580f5c4dfc3bab9bffd602303fea6f1
Ian Harris <ianmharris93@gmail.com>
2023-05-23 14:28:44 -0700

    merge

---
+++ Kalis/scripts/redirects.js
@@ -0,0 +1,13 @@
+
+let websiteKSF = ()=>{
+    window.open("https://www.knotsofunnypodcast.com/")
+}
+
+let websiteDrop = ()=>{
+    window.open("https://www.dropoutpizza.com")
+}
+
+let websiteKrit = ()=>{
+    window.open("https://www.krittercatchers.com")
+}
+
