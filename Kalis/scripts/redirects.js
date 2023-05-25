c7e3b46d3f5ef79ba5ff695c1d04546de7763158
ianm93 <45110918+iannm93@users.noreply.github.com>
2023-05-24 18:54:25 -0700

    added styles and graphics pages

--- Kalis/scripts/redirects.js
+++ Kalis/scripts/redirects.js
@@ -11,3 +11,10 @@ let websiteKrit = ()=>{
     window.open("https://www.krittercatchers.com")
 }
 
+let websiteHoney = ()=>{
+    window.open("https://online.fliphtml5.com/vnnyr/abxc/#p=1")
+}
+
+let websitePretty = ()=>{
+    window.open("https://heyzine.com/flip-book/08c238dc66.html")
+}
